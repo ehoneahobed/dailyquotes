145dfb938094470513e0e6336ac7b0a147184a56
ehoneahobed <ehoneahobed@hotmail.com>
2022-12-15 21:35:51 +0000

    Daily Quote MVP

---
+++ README.md
@@ -0,0 +1,17 @@
+# Daily Quotes - Quotes Generator
+This is a simple quotes generator that is powered by the type.fit api for quotes. This project was undertaken to enhance my basic HTML, CSS and Javascript skills.
+
+## How it Works
+On launch of the page, a quote is automatically generated. However, users can click on the `new quote` button to generate new quotes. Quotes generated can be shared directly to Twitter by clicking the `Tweet this quote` button.
+
+### Screenshot of Daily Quote webpage
+![Daily Quote Screenshot](./img/quote%20of%20the%20day.png)
+
+
+### Screenshot of sharing Daily Quote to Twitter
+![Daily Quote Screenshot](./img/tweet%20quote%20of%20the%20day.png)
+
+## Author
+Ehoneah Obed <ehoneahobed@hotmail.com>
+
+  Contact him via: [Twitter](https://ehoneahobed.com/twitter)
---
+++ main.js
@@ -0,0 +1,36 @@
+// accessing the DOM
+const quote_body = document.getElementById("quote");
+const quote_author = document.getElementById("author");
+const tweetButton = document.getElementById("tweet");
+
+const getQuote = async () => {
+    // access the api for the quotes
+    let url = "https://type.fit/api/quotes";
+
+    // fetch quotes from the api
+    const response = await fetch(url);
+
+    //  convert the response to json format
+    const allQuotes = await response.json();
+
+    const indx = Math.floor(Math.random() * allQuotes.length)
+
+    const quote = allQuotes[indx].text;
+
+    let author = allQuotes[indx].author;
+
+    if (author == null) {
+        author = "Anonymous";
+    }
+
+
+    // updating the content of the DOM
+    quote_body.innerHTML = quote;
+    quote_author.innerHTML = author;
+
+    //  tweet the quote
+    tweetButton.href="https://twitter.com/intent/tweet?text=Quote%20of%20the%20day!%0A"+quote+" - "+author+"%0ASource: https://blog.ehoneahobed.com";
+}
+
+
+getQuote();
